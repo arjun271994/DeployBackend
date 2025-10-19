d336825f372ac4d6a9b1007df4017d751b12554e
arjun271994 <arjun.shrivastava.27@gmail.com>
2025-10-19 12:47:22 +0530

    First commit after long

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>I am Server</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is runnin on http://localhost:3000");
+});
